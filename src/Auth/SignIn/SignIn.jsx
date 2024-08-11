4d58addfc58d59e9dcff224b6082fe340c32bfdb
MacBook Pro <naym100m@gmail.com>
2024-08-11 22:25:46 +0600

    [design] a signIn form use react hook form

--- src/Auth/SignIn/SignIn.jsx
+++ src/Auth/SignIn/SignIn.jsx
@@ -1,13 +1,76 @@
-
+import { useForm } from "react-hook-form";
+import SocialLogIn from "../SocialLogIn/SocialLogIn";
+import { Link } from "react-router-dom";
 
 const SignIn = () => {
-    return (
-        <>
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <>
+      <div className=" max-w-7xl w-7/12 mx-auto px-4 md:px-6 lg:px-10 mt-14">
+        <section className="">
+          <form onSubmit={handleSubmit(onSubmit)} className=" space-y-6">
             <div>
-                
+              <label>Email</label>
+              <input
+                className="mt-2 border w-full mx-auto rounded-md py-3 px-3 focus:outline-none"
+                {...register("email", { required: true })}
+                placeholder="Enter email"
+              />
+              {errors.email && (
+                <span className=" text-red-500 text-sm">
+                  This field is required
+                </span>
+              )}
             </div>
-        </>
-    );
+            <div>
+              <label> Password</label>
+              <input
+                className="mt-2 border w-full mx-auto rounded-md py-3 px-3 focus:outline-none"
+                {...register("password", { required: true })}
+                placeholder="Enter Password"
+              />
+              {errors.password && (
+                <span className=" text-red-500 text-sm">
+                  This field is required
+                </span>
+              )}
+            </div>
+            <button
+              type="button"
+              className="bg-[#055AB1] font-medium text-xl text-white rounded-md py-4 w-full text-center"
+            >
+              Sign In
+            </button>
+          </form>
+        </section>
+
+        <section>
+          <div className=" flex items-center text-center justify-center gap-3 my-8">
+            <div className=" border-b w-2/12"></div>
+            <p className=" font-medium text-gray-600">
+              Or, Sign in with your email
+            </p>
+            <div className=" border-b w-2/12"></div>
+          </div>
+          <SocialLogIn />
+          <div>
+            <p className=" font-medium text-gray-600 text-center my-11">
+              Do not have an account ?
+              <span className=" text-red-600 px-1">
+                <Link to="/signUp">Sign Up</Link>
+              </span>
+            </p>
+          </div>
+        </section>
+      </div>
+    </>
+  );
 };
 
 export default SignIn;
---
+++ src/Auth/SocialLogIn/SocialLogIn.jsx
@@ -0,0 +1,21 @@
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebook } from "react-icons/fa6";
+
+const SocialLogIn = () => {
+  return (
+    <>
+      <div className=" space-y-4">
+        <button className=" flex items-center gap-2 w-full mx-auto justify-center py-2 bg-gray-100">
+          <FcGoogle size={25} />
+          Sign up with Google
+        </button>
+        <button className=" flex items-center gap-2 w-full mx-auto justify-center py-2 bg-gray-100">
+          <FaFacebook size={25} />
+          Sign up with Facebook
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default SocialLogIn;
