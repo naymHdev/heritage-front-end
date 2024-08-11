929452292a0aad71c3a47d32409f87754e16f63f
MacBook Pro <naym100m@gmail.com>
2024-08-11 22:38:47 +0600

    [80%] compleate a sign in page

--- src/Auth/SignIn/SignIn.jsx
+++ src/Auth/SignIn/SignIn.jsx
@@ -1,6 +1,7 @@
 import { useForm } from "react-hook-form";
 import SocialLogIn from "../SocialLogIn/SocialLogIn";
 import { Link } from "react-router-dom";
+import logo from "../../assets/Hertiage Nest - Final LOGO (1) 1.png";
 
 const SignIn = () => {
   const {
@@ -12,13 +13,25 @@ const SignIn = () => {
 
   return (
     <>
-      <div className=" max-w-7xl w-7/12 mx-auto px-4 md:px-6 lg:px-10 mt-14">
-        <section className="">
+      <div className=" max-w-7xl md:w-7/12 mx-auto px-4 md:px-6 lg:px-10 mt-8">
+        <section>
+          <div className=" flex items-center gap-2 justify-center">
+            <img className=" w-12 h-full" src={logo} alt="website logo" />
+            <h2 className=" text-3xl font-extrabold">
+              Heritage<span className="text-[#055BB2]">Nest</span>
+            </h2>
+          </div>
+          <h1 className=" text-2xl md:text-4xl font-bold mt-8 md:mt-14">
+            Hey There!!! Welcome Back.
+          </h1>
+        </section>
+
+        <section className="mt-8">
           <form onSubmit={handleSubmit(onSubmit)} className=" space-y-6">
             <div>
               <label>Email</label>
               <input
-                className="mt-2 border w-full mx-auto rounded-md py-3 px-3 focus:outline-none"
+                className="mt-2 focus:border bg-[#F6F6F9] focus:bg-inherit w-full mx-auto rounded-md py-3 px-3 focus:outline-none"
                 {...register("email", { required: true })}
                 placeholder="Enter email"
               />
@@ -31,7 +44,7 @@ const SignIn = () => {
             <div>
               <label> Password</label>
               <input
-                className="mt-2 border w-full mx-auto rounded-md py-3 px-3 focus:outline-none"
+                className="mt-2 focus:border bg-[#F6F6F9] focus:bg-inherit w-full mx-auto rounded-md py-3 px-3 focus:outline-none"
                 {...register("password", { required: true })}
                 placeholder="Enter Password"
               />
@@ -41,12 +54,14 @@ const SignIn = () => {
                 </span>
               )}
             </div>
-            <button
-              type="button"
-              className="bg-[#055AB1] font-medium text-xl text-white rounded-md py-4 w-full text-center"
-            >
-              Sign In
-            </button>
+            <div>
+              <button
+                type="button"
+                className="bg-[#055AB1] font-medium text-xl text-white rounded-md py-4 w-full text-center"
+              >
+                Sign In
+              </button>
+            </div>
           </form>
         </section>
 
