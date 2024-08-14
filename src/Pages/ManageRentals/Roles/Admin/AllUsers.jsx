7dbaca0fa895e49cf395b8c9cb47e0eec2702663
MacBook Pro <naym100m@gmail.com>
2024-08-14 11:09:48 +0600

    [added] a user deleted function in user management admin route

--- src/Pages/ManageRentals/Roles/Admin/AllUsers.jsx
+++ src/Pages/ManageRentals/Roles/Admin/AllUsers.jsx
@@ -3,17 +3,17 @@ import useAllUsers from "../../../../Hooks/useAllUsers";
 import UserCard from "./UserCard";
 
 const AllUsers = () => {
-  const [allUsers] = useAllUsers();
+  const [allUsers, refetch] = useAllUsers();
 
   return (
     <>
-      <div>
+      <div className=" min-h-screen">
         <DashBoardSectionName title="All Users" lengths={allUsers.length} />
 
         <section className=" mt-8">
           <div className=" grid grid-cols-1 md:grid-cols-3 gap-8">
             {allUsers?.map((user) => (
-              <UserCard key={user?._id} user={user} />
+              <UserCard key={user?._id} user={user} refetch={refetch} />
             ))}
           </div>
         </section>
--- src/Pages/ManageRentals/Roles/Admin/UserCard.jsx
+++ src/Pages/ManageRentals/Roles/Admin/UserCard.jsx
@@ -1,31 +1,48 @@
 /* eslint-disable react/prop-types */
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
-import { Card } from "antd";
-const { Meta } = Card;
+import publicAxios from "../../../../Hooks/PublicAxios";
+import toast from "react-hot-toast";
 
-const UserCard = ({ user }) => {
-  // console.log("user", user);
+const UserCard = ({ user, refetch }) => {
+  const { email, name, roles, _id } = user || {};
 
-  const { email, name } = user || {};
+  const handleDelete = async (id) => {
+    try {
+      const response = await publicAxios.delete(`/api/signUp/${id}`);
+      if (response.data.deletedCount == 1) {
+        toast.success("User deleted successful");
+        refetch();
+      } else {
+        toast.error("User deleted failed!");
+      }
+    } catch (error) {
+      console.log("user deleted", error);
+    }
+  };
 
   return (
     <>
       <div>
-        <Card
-          className=" w-full"
-          cover={
-            <img
-              alt="example"
-              src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-            />
-          }
-          actions={[
-            <EditOutlined key="edit" />,
-            <DeleteOutlined key="delete" />,
-          ]}
-        >
-          <Meta title={name} description={email} />
-        </Card>
+        <div className=" w-full p-6 bg-white rounded-2xl border-none">
+          <div className=" space-y-1">
+            <h2 className=" text-xl font-bold text-black">{name}</h2>
+            <p className=" text-gray-700 font-medium">{email}</p>
+            <p className=" text-gray-700 font-medium">{roles}</p>
+          </div>
+          <div className=" border-b w-full my-6" />
+          <div className=" flex items-center justify-between">
+            <button>
+              <EditOutlined key="edit" />
+            </button>
+            <button
+              onClick={() => {
+                handleDelete(_id);
+              }}
+            >
+              <DeleteOutlined key="delete" />
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
